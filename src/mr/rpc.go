1ab52051020f2b414d08d1ae18c908d6f7eab770
Pavan rajasekhar <pavanrajasekhar@gmail.com>
2023-08-25 12:41:26 +0530

    github cred setup

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,13 +6,13 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
-//
 // example to show how to declare the arguments
 // and reply for an RPC.
-//
 
 type ExampleArgs struct {
 	X int
@@ -23,7 +23,6 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
